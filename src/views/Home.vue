f0f616c6592a9bbaf9d1d6e8ae1d37b897fabcb7
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-10-22 14:25:27 -0500

    10.22.18

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Interesting People</h1>
+
+    <div>
+      Name: <input v-model="newPerson.name">  
+      Bio: <input v-model="newPerson.bio">
+      <button v-on:click="addPerson()">Add Person</button>
+    </div>
+
+    <div>
+      <h4>Total Number of People: {{ people.length }}</h4>
+    </div>
+
+    <div v-for="person in people">
+      <h2 @click="toggleBio(person)">{{ person.name }}</h2>
+      <div v-bind:class="{strike: person.bioVisible, pumpkin: true}">
+        <h3>{{ person.bio }}</h3>
+        <button @click="deletePerson(person)">Delete</button>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+.strike {
+  text-decoration: line-through;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      people: [],
+      newPerson: {name: "", bio: "", bioVisible: true}
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/people")
+    .then(function(response) {
+      console.log
+        this.people = response.data;
+    }.bind(this));
+},
+  methods: {
+    toggleBio: function(inputPerson) {
+      inputPerson.bioVisible = !inputPerson.bioVisible;
+    },
+  addPerson: function() {
+
+          var params = {
+                        name: this.newPerson.name,
+                        bio: this.newPerson.bio
+                        };
+
+  axios 
+  .post("http://localhost:3000/api/people", params)
+  .then(function(response){
+    console.log(response.data)
+    this.people.push(this.newPerson);
+  }.bind(this));
+
+          this.newPerson = {name: "", bio: "", bioVisible: false};
+        
+      },
+    },
+    deletePerson: function(inputPerson) {
+      var index = this.people.indexOf(inputPerson);
+      this.people.splice(index, 1);
+    }
+  },
+  computed: {}
+};
 </script>
